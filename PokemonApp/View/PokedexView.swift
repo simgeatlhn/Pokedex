7d678b9445c56471e5e914fbd283d6fdc6b99468
simgeatlhn <simgeeatlihan@gmail.com>
2022-11-27 14:13:39 +0300

    added pokedexview

--- PokemonApp/View/PokedexView.swift
+++ PokemonApp/View/PokedexView.swift
@@ -8,10 +8,22 @@
 import SwiftUI
 
 struct PokedexView: View {
+    private let gridItems = [GridItem(.flexible()), GridItem(.flexible())] //only two items in row
+    
     var body: some View {
-        Text("PokedexView")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: gridItems, spacing: 16) {
+                    ForEach(0..<100) { _ in
+                        PokemonCell()
+                    }
+                }
+            }
+            .navigationTitle("Pokemon")
+        }
     }
 }
+
 
 struct PokedexView_Previews: PreviewProvider {
     static var previews: some View {
